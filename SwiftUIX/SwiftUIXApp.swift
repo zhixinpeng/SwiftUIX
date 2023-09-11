c546ddeca81ada23d72847360be5ee13ab1dc90b
Angus <pengzhixin@live.com>
2023-09-11 17:40:48 +0800

    feat: initial commit

---
+++ SwiftUIX/SwiftUIXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIXApp.swift
+//  SwiftUIX
+//
+//  Created by Angus on 2023/9/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
